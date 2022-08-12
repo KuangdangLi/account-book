a90914bda6dab5553d0c34b09276811949151f75
riverjoy <tosakarinmylove@gmail.com>
2022-08-12 16:53:40 +0800

    引入NotFound页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, { RouteConfig } from 'vue-router'
 import Money from '@/views/Money.vue'
 import Label from '@/views/Label.vue'
 import Statistics from '@/views/Statistics.vue'
+import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -26,6 +27,11 @@ const routes: Array<RouteConfig> = [
     name: 'Statistics',
     component: Statistics
   },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        未找到页面，请检查输入的网址是否正确。
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
