1db71a74abd0d16110b9b049ccd213a4f8992083
riverjoy <tosakarinmylove@gmail.com>
2022-08-12 21:54:59 +0800

    修改config批量引入svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,26 @@
 <template lang="">
     <div class="nav">
     <router-link to="/money">Money</router-link>
-    <router-link to="/label">Label</router-link>
+    <router-link to="/label">
+        <svg>
+            <use xlink:href="#labels"/>
+        </svg>
+        Label</router-link>
     <router-link to="/statistics">Statistics</router-link>
     </div>
 </template>
 
 <script lang="ts">
-export default {
-
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+try {
+    importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (error) {
+    console.log(error)
 }
+
+export default {
+    name: 'Nav'
+};
 </script>
 
 
