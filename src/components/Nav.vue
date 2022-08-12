0484bc786fe9edc9483d4dfca26c409f0b0f48e6
riverjoy <tosakarinmylove@gmail.com>
2022-08-12 22:28:16 +0800

    创建Icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,18 @@
 <template lang="">
     <div class="nav">
-    <router-link to="/money">Money</router-link>
+    <router-link to="/money">
+        <Icon name="money"/>
+       <p> Money</p></router-link>
     <router-link to="/label">
-        <svg>
-            <use xlink:href="#labels"/>
-        </svg>
-        Label</router-link>
-    <router-link to="/statistics">Statistics</router-link>
+        <Icon name="labels"/>
+       <p> Label</p></router-link>
+    <router-link to="/statistics">
+        <Icon name="chart"/>
+        <p>Statistics</p></router-link>
     </div>
 </template>
 
 <script lang="ts">
-let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
-try {
-    importAll(require.context('../assets/icons', true, /\.svg$/));
-} catch (error) {
-    console.log(error)
-}
-
 export default {
     name: 'Nav'
 };
@@ -27,5 +22,7 @@ export default {
 <style lang="scss" scoped>
 .nav {
     border: 1px solid red;
+    display: flex;
+    flex-direction: row;
 }
 </style>
