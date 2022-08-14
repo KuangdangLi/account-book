bbffce7ff2e6963a16f99a64e9023cbe01606dfa
riverjoy <tosakarinmylove@gmail.com>
2022-08-14 16:33:28 +0800

    总体布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,29 +1,6 @@
 <template>
     <div>
-        <Layout>
-            <div class="tags">
-                <ul class="current">
-                    <li>衣</li>
-                    <li>食</li>
-                    <li>住</li>
-                    <li>行</li>
-                </ul>
-                <div class="new">
-                    <button>新增标签</button>
-                </div>
-            </div>
-            <div>
-                <label class="notes">
-                    <span class="name">备注</span>
-                    <input type="text" placeholder="在这里添加备注">
-                </label>
-            </div>
-            <div>
-                <ul class="types">
-                    <li class="selected">支出</li>
-                    <li>收入</li>
-                </ul>
-            </div>
+        <Layout classPrefix="layout">
             <div class="numberPad">
                 <div class="output">100</div>
                 <div class="buttons">
@@ -38,9 +15,32 @@
                     <button>7</button>
                     <button>8</button>
                     <button>9</button>
-                    <button>OK</button>
-                    <button>0</button>
+                    <button class="ok">OK</button>
+                    <button class="zero">0</button>
                     <button>.</button>
+                </div>
+            </div>
+            <div>
+                <ul class="types">
+                    <li class="selected">支出</li>
+                    <li>收入</li>
+                </ul>
+            </div>
+            <div>
+                <label class="notes">
+                    <span class="name">备注</span>
+                    <input type="text" placeholder="在这里添加备注">
+                </label>
+            </div>
+            <div class="tags">
+                <ul class="current">
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
+                </ul>
+                <div class="new">
+                    <button>新增标签</button>
                 </div>
             </div>
         </Layout>
@@ -56,13 +56,26 @@ export default {
 }
 </script>
 
+<style lang="scss">
+.layout-content {
+    display: flex;
+    flex-direction: column-reverse;
+}
+</style>
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
 .tags {
     font-size: 14px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column-reverse;
 
     >.current {
         display: flex;
         flex-direction: row;
+        order: 2;
+        flex-wrap: wrap;
 
         >li {
             background-color: #D9D9D9;
@@ -76,6 +89,7 @@ export default {
     }
 
     >.new {
+        order: 1;
         padding-top: 16px;
         padding-bottom: 12px;
         padding-left: 18px;
@@ -139,10 +153,68 @@ export default {
 
 .numberPad {
     .output {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        height: 72px;
         font-size: 36px;
         font-family: Consolas, monospace;
+        padding-right: 16px;
+        box-shadow: 0px -2px 3px 0px fade_out(#000, 0.75) inset;
     }
 
-    .buttons {}
+    .buttons {
+        $h: 7.8vh;
+
+        >button {
+            @extend %clearfix;
+            width: 25%;
+            height: $h;
+            background-color: transparent;
+            border: none;
+            $b: #F2F2F2;
+
+            &.ok {
+                height: $h*2;
+                float: right;
+            }
+
+            &.zero {
+                width: 50%;
+            }
+
+            &:nth-child(1) {
+                background-color: $b;
+            }
+
+            &:nth-child(2),
+            &:nth-child(5) {
+                background-color: darken($b, 4%);
+            }
+
+            &:nth-child(3),
+            &:nth-child(6),
+            &:nth-child(9) {
+                background-color: darken($b, 4%*2);
+            }
+
+            &:nth-child(4),
+            &:nth-child(7),
+            &:nth-child(10),
+            &:nth-child(13) {
+                background-color: darken($b, 4%*3);
+            }
+
+            &:nth-child(8),
+            &:nth-child(11) {
+                background-color: darken($b, 4%*4);
+            }
+
+            &:nth-child(12),
+            &:nth-child(14) {
+                background-color: darken($b, 4%*5);
+            }
+        }
+    }
 }
 </style>
