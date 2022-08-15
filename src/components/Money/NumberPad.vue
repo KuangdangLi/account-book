ffbcc4126e3b796a85fbacb800ada10d82506ba4
riverjoy <tosakarinmylove@gmail.com>
2022-08-15 14:39:44 +0800

    NumberPad完成

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="numberPad">
-        <div class="output">100</div>
+        <div class="output">{{ output }}</div>
         <div class="buttons">
             <button>1</button>
             <button>2</button>
             <button>3</button>
-            <button>删除</button>
+            <button @click="remove">删除</button>
             <button>4</button>
             <button>5</button>
             <button>6</button>
-            <button>清空</button>
+            <button @click="clear">清空</button>
             <button>7</button>
             <button>8</button>
             <button>9</button>
@@ -19,6 +19,82 @@
         </div>
     </div>
 </template>
+
+<!-- <script lang="ts">
+import Vue from "Vue"
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class NumberPad extends Vue {
+    output = '0';
+    inputContent(e: MouseEvent) {
+        const button = e.target as HTMLButtonElement;
+        const input = button.textContent!;
+        if (this.output === '0') {
+            if ('0123456789'.indexOf(input) >= 0) {
+                this.output = input
+            } else {
+                this.output += input
+            }
+        } else if (this.output.indexOf('.') >= 0) return
+        else this.output += input
+    }
+    created() {
+        const li = document.querySelectorAll('div.buttons>button')
+        console.log(li);
+
+    }
+}
+
+</script> -->
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+@Component
+export default class NumberPad extends Vue {
+    output = '0';
+    inputContent(e: Event) {
+        const button = e.target as HTMLButtonElement;
+        const input = button.textContent!;
+        if (this.output.length === 16) return
+        if (this.output === '0') {
+            if ('0123456789'.indexOf(input) >= 0) {
+                this.output = input
+            } else {
+                this.output += input
+            }
+        } else if (this.output.indexOf('.') >= 0) {
+            if (input === '.') return
+            else {
+                this.output += input
+            }
+        } else { this.output += input }
+
+    }
+    remove() {
+        if (this.output.length > 1) {
+            this.output = this.output.slice(0, -1)
+        } else {
+            this.output = '0'
+        }
+    }
+    clear() {
+        this.output = '0'
+    }
+    mounted() {
+        let li = document.querySelectorAll('div.buttons>button')
+        li.forEach((item) => {
+            if ('0123456789.'.indexOf(item.innerHTML) >= 0) {
+                item.addEventListener('click', (e) => {
+                    this.inputContent(e)
+                })
+            }
+        })
+    }
+}
+</script>
+
+
 
 <style lang="scss">
 @import "~@/assets/style/helper.scss";
