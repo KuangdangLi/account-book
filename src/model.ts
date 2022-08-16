b328049ef01a422adfe5ce8b1fae207f65f57bd1
riverjoy <tosakarinmylove@gmail.com>
2022-08-16 16:13:15 +0800

    model增加clone方法

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,8 @@
 const localStorageKeyName = "recordList";
 const model = {
+  clone(data:RecordItem):RecordItem{
+    return JSON.parse(JSON.stringify(data))
+  },
   fetch(): RecordItem[]| []{
     return JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]");
   },
