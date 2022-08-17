52e12eae553f15d2b626cdbae617e23c704a5060
riverjoy <tosakarinmylove@gmail.com>
2022-08-17 21:55:41 +0800

    修改了由tag数据结构变化出现的展示bug，还有bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="tags">
         <ul class="current">
-            <li v-for="tag in dataSource" :key="tag" @click="toggle(tag)"
-                :class="{ selected: selectedTags.indexOf(tag) >= 0 }">{{ tag }}</li>
+            <li v-for="tag in dataSource" :key="tag.id" @click="toggle(tag)"
+                :class="{ selected: selectedTags.indexOf(tag) >= 0 }">{{ tag.name }}</li>
         </ul>
         <div class="new">
             <button @click="create">新增标签</button>
