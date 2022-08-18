b01dd085ad5e309747593bc3e86486bc70d66b10
riverjoy <tosakarinmylove@gmail.com>
2022-08-18 22:20:00 +0800

    修复类型bug

--- src/views/Label.vue
+++ src/views/Label.vue
@@ -1,60 +1,62 @@
 <template>
-    <div>
-        <Layout>
-            <div class="tags">
-                  <router-link :to="`/label/edit/${tag.id}`" v-for="tag in tags" :key="tag.id" class="tag"><span>{{tag.name}}</span>
-                        <Icon name="right"></Icon>
-                    </router-link>
-            </div>
-            <div class="button-wrapper">
-                <Button  @click="createTag">新增标签</Button>
-            </div>
-        </Layout>
-    </div>
+  <div>
+    <Layout>
+      <div class="tags">
+        <router-link :to="`/label/edit/${tag.id}`" v-for="tag in tags" :key="tag.id" class="tag">
+          <span>{{ tag.name }}</span>
+          <Icon name="right"></Icon>
+        </router-link>
+      </div>
+      <div class="button-wrapper">
+        <Button @click="createTag">新增标签</Button>
+      </div>
+    </Layout>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
 import store from '@/store/store2';
 @Component({
-  components: {Button}
+  components: { Button }
 })
 export default class Label extends Vue {
   tags = store.tagList
-  createTag(){
-    const name =window.prompt('请输入标签名')
-    if(name) {
+  createTag(): void {
+    const name = window.prompt('请输入标签名')
+    if (name) {
       try {
         store.createTag(name)
-      } catch (error) {
+      } catch (error: Error) {
         window.alert(`${error.message}`)
       }
-    }else{
-    window.alert(`the label cannot be empty`)
-  }}
+    } else {
+      window.alert(`the label cannot be empty`)
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .tags {
 
-    padding-left: 15px;
+  padding-left: 15px;
 
-    >.tag {
-        display: flex;
-        min-height: 44px;
-        justify-content: space-between;
-        align-items: center;
-        border-bottom: 1px solid #dddde0;
+  >.tag {
+    display: flex;
+    min-height: 44px;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #dddde0;
 
-        svg.icon {
-            width: 20px;
-            height: 20px;
-            margin-right: 23px;
-        }
+    svg.icon {
+      width: 20px;
+      height: 20px;
+      margin-right: 23px;
     }
+  }
 }
 
 //.createTag {
